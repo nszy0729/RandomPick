f406c540d6b2b4726828ed2a36a4ab0155eea383
nszy0729 <nszy0729@gmail.com>
2025-02-09 16:45:58 +0900

    Fix エラーハンドリングを見直し

--- commands/pick.js
+++ commands/pick.js
@@ -12,16 +12,20 @@ module.exports = {
       .setDescription('Number of people to pick.')
       .setRequired(true)),
 	execute: async function(interaction) {
-    const targets = interaction.options.getString('targets').split(',');
-    const pickCount = interaction.options.getInteger('pickcount');
-    // スラッシュコマンドのオプションを検証する
-    const validateMessage = util.validateOptions(targets, pickCount);
-    if (validateMessage) {
-		  await interaction.reply(validateMessage);
-      return;
+    try {
+      const targets = interaction.options.getString('targets').split(',');
+      const pickCount = interaction.options.getInteger('pickcount');
+      // スラッシュコマンドのオプションを検証する
+      const validateMessage = util.validateOptions(targets, pickCount);
+      if (validateMessage) {
+        await interaction.reply(validateMessage);
+        return;
+      }
+      // 対象をランダムにピックして返す
+      const picked = util.pickItems(targets, pickCount);
+      await interaction.reply({ embeds: [util.generateReplyEmbed(targets, pickCount, picked)] });
+    } catch(e) {
+      console.log('reply時にエラー', e);
     }
-    // 対象をランダムにピックして返す
-    const picked = util.pickItems(targets, pickCount);
-    await interaction.reply({ embeds: [util.generateReplyEmbed(targets, pickCount, picked)] });
 	},
 };
